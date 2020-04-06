84add623b50b6b3ed2d6939084f76cc22628502b
Badr Eddine <bin.benh@gmail.com>
2020-04-06 15:51:35 +0100

    added color to cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -19,7 +19,7 @@ function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
             <Grid container spacing={3} justify="center" >
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
-                        <Typography color='textSecondary' gutterBottom >Infected</Typography>
+                        <Typography color='textSecondary' className={styles.blue} gutterBottom >Infected</Typography>
                         <Typography variant='h4'  >
                             <CountUp
                                 start={0}
@@ -34,7 +34,7 @@ function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
-                        <Typography color='textSecondary' gutterBottom >Recovered</Typography>
+                        <Typography className={styles.green} color='textSecondary' gutterBottom >Recovered</Typography>
                         <Typography variant='h4'  >
                             <CountUp start={0}end={recovered.value}duration = {3}separator=","/>
                         </Typography>
@@ -44,7 +44,7 @@ function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
-                        <Typography color='textSecondary' gutterBottom  >Deaths</Typography>
+                        <Typography className={styles.red} color='textSecondary' gutterBottom  >Deaths</Typography>
                         <Typography variant='h4'  >
                             <CountUp start={0} end={deaths.value} duration = {3}separator=","/>
                         </Typography>
